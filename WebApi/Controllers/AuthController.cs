e0d2742e897163366ce4381096042dade7c331f2
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2025-09-24 20:24:13 +0200

    Add profile endpoint and update emmebrship endpoint and authorization

--- WebApi/Controllers/AuthController.cs
+++ WebApi/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 using WebApi.Interfaces;
 using WebApi.Models;
 
---
+++ WebApi/Models/ProfileModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Models;
+
+public class ProfileModel
+{
+    public string UserId { get; set; } = null!;
+
+    public int MembershipId { get; set; }
+
+    public string Email { get; set; } = null!;
+}
