3e45ec29126c8ff98784929b19171f50c1587c2b
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2025-09-30 20:30:56 +0200

    add bool if user has active subscription in user res

--- WebApi/Controllers/AuthController.cs
+++ WebApi/Controllers/AuthController.cs
@@ -50,7 +50,9 @@ public class AuthController(IAuthService authService, IAccessTokenService access
             if (string.IsNullOrEmpty(accesstoken))
                 return StatusCode(500, "Failed To create the access token");
 
-            return Ok(new { success = true, message = "You Signed in successfully", token = accesstoken });
+            bool hasActiveSubscription = user.SubscriptionStatus == "active";
+
+            return Ok(new { success = true, message = "You Signed in successfully", token = accesstoken, hasActiveSubscription = hasActiveSubscription });
 
         } catch (Exception ex)
         {
